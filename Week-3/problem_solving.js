88a3cbd25daa1fd725d38909be5c5b28f5670504
cjhadev <cjha2feb@gmail.com>
2022-05-27 21:34:42 +0530

    Added the comment

--- Week-3/problem_solving.js
+++ Week-3/problem_solving.js
@@ -12,5 +12,9 @@ function createIncrement() {
 const[increment,log] = createIncrement();
 increment();
 increment();
-increment();l
+increment();
 log();// What is logged?
+//Output -> Count is 0;
+//Count is zero because increment function is only changing the count but not message
+//if we need to change the message too then we have to put that message inside the increment function
+

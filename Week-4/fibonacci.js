3eef7f65c55f476ae4a7a97194895317514f57a5
cjhadev <cjha2feb@gmail.com>
2022-06-05 15:38:11 +0530

    fibonacci function is created using iterators

---
+++ Week-4/fibonacci.js
@@ -0,0 +1,18 @@
+const fib = (n) => ({
+    [Symbol.iterator]: () => {
+        let i = 0;
+        let old = 0; 
+        let new1 = 0;
+        return {
+            next: () => {
+                if(i++ < n){
+                    [old, new1] = [new1, (old +new1) || 1]
+                    return {value: old, done: false}
+                }else{
+                    return { done: true}
+                }
+            }
+        }
+    }
+})
+console.log([...fib(10)]);
